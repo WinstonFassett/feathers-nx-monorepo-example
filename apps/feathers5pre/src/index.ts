6b504347e95fc99ab26fa37d70e1846c151d303e
Winston Fassett <winston.fassett@gmail.com>
2022-12-25 19:14:55 -0600

    Add package dep

--- apps/feathers5pre/src/index.ts
+++ apps/feathers5pre/src/index.ts
@@ -1,5 +1,9 @@
+import { isOdd } from 'is-odd'
 import { app } from './app'
 import { logger } from './logger'
+
+const is1Odd = isOdd(1)
+console.log({ is1Odd })
 
 const port = app.get('port')
 const host = app.get('host')
